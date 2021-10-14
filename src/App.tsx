d07b945f9290cff052994af2c1081590b7979836
Guillermo Martinez Espina <gmotzespina@icloud.com>
2021-10-14 18:35:16 +0100

    Adds PDFtron and searches for a string

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,64 @@
 import "./App.css";
-import PDFViewer from "./components/pdf-viewer";
+import WebViewer from "@pdftron/pdfjs-express-viewer";
+import { useEffect, useRef, useState } from "react";
+
+interface CoreInstance {
+  clearSearchResults: () => null;
+  UI: {
+    searchTextFull: (pattern: String, options?: any) => null;
+  };
+}
 
 function App() {
+  const viewer = useRef(null);
+  const [coreInstance, setCoreInstance] = useState<CoreInstance>();
+  const [UIInstance, setUIInstance] = useState<any>();
+
+  // const docViewer = new DocumentViewer();
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: "/webviewer/lib",
+        initialDoc: "/assets/test.pdf",
+        licenseKey: "ThTZn5MlPR1UiopS0hmK",
+      },
+      viewer.current
+    ).then((instance) => {
+      // now you can access APIs through the WebViewer instance
+      const { Core, UI } = instance;
+
+      setCoreInstance(Core.documentViewer);
+      setUIInstance(UI);
+
+      Core.documentViewer.zoomTo(1, 0, 0);
+      // adding an event listener for when a document is loaded
+      Core.documentViewer.addEventListener("documentLoaded", () => {
+        console.log("document loaded");
+      });
+    });
+  }, []);
+
+  const onClearSearchResults = () => {
+    if (coreInstance) {
+      coreInstance.clearSearchResults();
+    }
+  };
+
+  const onSearch = () => {
+    if (coreInstance) {
+      console.log(UIInstance.searchTextFull("Sex", { caseSensitive: true }));
+    }
+  };
+
   return (
     <div className="sidebar">
       <header className="App-header">
         <p>This is my chrome extension</p>
-        <PDFViewer />
+        <button onClick={onClearSearchResults}>clearSearchResults</button>
+        <button onClick={onSearch}>perform search</button>
       </header>
+      <div className="webviewer" ref={viewer}></div>
     </div>
   );
 }
